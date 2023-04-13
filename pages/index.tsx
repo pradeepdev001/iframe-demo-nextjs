90a5c7a8bac6256a7e66f212d450be17937ddfd9
pradeepdev001 <94212000+pradeepdev001@users.noreply.github.com>
2023-04-13 13:12:04 +0530

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Radio, Button } from "@material-tailwind/react";
+import React, { useEffect, useRef, useState } from 'react';
+import { Radio, Button } from '@material-tailwind/react';
 
 export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
   const gridIframe = useRef(null);
-  const [urlState, setUrlState] = useState("http://localhost:3000/sample");
-  const [sampleIdState, setSampleIdState] = useState("");
+  const [urlState, setUrlState] = useState('sample');
+  const [sampleIdState, setSampleIdState] = useState('');
 
-  useEffect(() => {
-
-  }, []);
+  useEffect(() => {}, []);
 
   const setUrl = (e: any) => {
     setUrlState(e.target.value);
-  }
+  };
 
   function handleIframe() {
     try {
       const iframeItem = gridIframe?.current;
-      const anchors = iframeItem?.contentWindow?.document?.getElementsByTagName("a");
+      const anchors =
+        iframeItem?.contentWindow?.document?.getElementsByTagName('a');
     } catch (error) {
-      alert("Url can not be load in iframe");
+      alert('Url can not be load in iframe');
     }
   }
 
   function getSampleID() {
     try {
       const iframeItem = gridIframe?.current;
-      const data = iframeItem?.contentWindow?.document?.getElementById("sample_id");
+      const data =
+        iframeItem?.contentWindow?.document?.getElementById('sample_id');
       setSampleIdState(data?.textContent);
     } catch (error) {
-      console.log("getSampleID Error:", error);
+      console.log('getSampleID Error:', error);
     }
   }
 
@@ -38,35 +38,43 @@ export default function Modal() {
     <>
       <main className="flex min-h-screen flex-col items-center  p-24">
         <div className="flex gap-10">
-          <Radio defaultChecked onChange={setUrl} name="url" value={"http://localhost:3000/sample"} label="http://localhost:3000/sample" />
+          <Radio
+            defaultChecked
+            onChange={setUrl}
+            name="url"
+            value={'sample'}
+            label="sample"
+          />
         </div>
 
         <div className="flex gap-10">
-          <Radio onChange={setUrl} name="url" value={"https://www.canada.ca/en/immigration-refugees-citizenship/services/come-canada-tool.html"} label="https://www.canada.ca/en/immigration-refugees-citizenship/services/come-canada-tool.html" />
+          <Radio
+            onChange={setUrl}
+            name="url"
+            value={
+              'https://www.canada.ca/en/immigration-refugees-citizenship/services/come-canada-tool.html'
+            }
+            label="https://www.canada.ca/en/immigration-refugees-citizenship/services/come-canada-tool.html"
+          />
         </div>
 
-        <Button
-          onClick={() => setShowModal(true)}
-        >
-          Show modal
-        </Button>
+        <Button onClick={() => setShowModal(true)}>Show modal</Button>
 
         {showModal ? (
           <>
-            <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            >
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
               <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
                   <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">
-                      Modal Title
-                    </h3>
+                    <h3 className="text-3xl font-semibold">Modal Title</h3>
                     <button
                       className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => { setShowModal(false); setSampleIdState(""); }}
+                      onClick={() => {
+                        setShowModal(false);
+                        setSampleIdState('');
+                      }}
                     >
                       <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                         ×
@@ -91,15 +99,14 @@ export default function Modal() {
                     <button
                       className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
-                      onClick={() => { setShowModal(false); setSampleIdState(""); }}
+                      onClick={() => {
+                        setShowModal(false);
+                        setSampleIdState('');
+                      }}
                     >
                       Close
                     </button>
-                    <Button
-                      onClick={getSampleID}
-                    >
-                      Get Sample ID
-                    </Button>
+                    <Button onClick={getSampleID}>Get Sample ID</Button>
                   </div>
                 </div>
               </div>
